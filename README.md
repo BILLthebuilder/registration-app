183bcdf813d7fa1e3c901632e0dd3430003f5e26
BILLthebuilder <billkariri@gmail.com>
2019-10-22 20:45:34 +0300

    add all necessary files

---
+++ README.md
@@ -0,0 +1,30 @@
+# registration-app
+
+This is a user login and registration app using Node.js, Express, Passport, Mysql , Sequelize ORM, EJS and some other packages.
+
+## Version: 1.0.0
+
+### Usage
+
+```sh
+
+npm install
+
+```
+
+```sh
+$ npm start
+# Or run with Nodemon
+npm run dev
+
+Visit http://localhost:5000
+```
+
+### Mysql
+
+Make sure mysql is installed and configured on your system
+Create a database of your choice and then refer to the `.env.example` file to make your database work
+
+### Open source
+
+Feel free to use this as a biolerplate code for your site/webapp projects :)
---
+++ models/user.js
@@ -0,0 +1,48 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize({
+  database: process.env.DB,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  dialect: 'mysql'
+});
+
+
+const User = sequelize.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  password: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  date: {
+    type: Sequelize.DATEONLY,
+
+  },
+});
+
+// Connect to Mysql
+sequelize
+  .authenticate(
+    // db,
+    // { useNewUrlParser: true }
+  )
+  .then(() => console.log('Mysql Connected'))
+  .catch(err => console.log(err));
+
+User.sync()
+  .then(() => console.log('User table created succesfully'))
+  .catch(err => console.log('Unable to create the user table'));
+
+
+//const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
+
